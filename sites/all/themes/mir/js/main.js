005f493b5b86e7524d86c5dfde5523412e86201f
abumalick <abdullah.hilson@gmail.com>
2017-02-04 23:32:53 +0200

    style

--- sites/all/themes/mir/js/main.js
+++ sites/all/themes/mir/js/main.js
@@ -1,40 +1,39 @@
 var Drupal = Drupal || {};
-(function ($,Drupal) {
+(function ($, Drupal) {
   Drupal.behaviors.mir = {
-    attach: function (context, settings){
-			function play(audiofile){
-				var audio_player = document.getElementById('audio-player');
-				audio_player.removeAttribute("src");
-			 	audio_player.src = audiofile;
-			 	audio_player.play();
-			}
+    attach: function (context, settings) {
+      function play(audiofile) {
+        var audioPlayer = document.getElementById('audio-player');
+        audioPlayer.removeAttribute('src');
+        audioPlayer.src = audiofile;
+        audioPlayer.play();
+      }
 
-	    $('.field-name-field-audio a, a.play').on('click', function(e){
-	    	e.preventDefault();
-	    	play($(this).attr('href'));
-	    });
-		// jplayer:
-		$(document).ready(function(){
-      $("#jquery_jplayer_1").jPlayer({
-        ready: function () {
-          $(this).jPlayer("setMedia", {
-            title: "Bubble",
-            m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
-            oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
-          });
-        },
-        cssSelectorAncestor: "#jp_container_1",
-        swfPath: "/js",
-        supplied: "m4a, oga",
-        useStateClassSkin: true,
-        autoBlur: false,
-        smoothPlayBar: true,
-        keyEnabled: true,
-        remainingDuration: true,
-        toggleDuration: true
+      $('.field-name-field-audio a, a.play').on('click', function (e) {
+        e.preventDefault();
+        play($(this).attr('href'));
       });
-    });
-
-		}
-  }
-})(jQuery,Drupal);
+      // jplayer:
+      $(document).ready(function () {
+        $('#jquery_jplayer_1').jPlayer({
+          ready: function () {
+            $(this).jPlayer('setMedia', {
+              title: 'Bubble',
+              m4a: 'http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a',
+              oga: 'http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg'
+            });
+          },
+          cssSelectorAncestor: '#jp_container_1',
+          swfPath: '/js',
+          supplied: 'm4a, oga',
+          useStateClassSkin: true,
+          autoBlur: false,
+          smoothPlayBar: true,
+          keyEnabled: true,
+          remainingDuration: true,
+          toggleDuration: true
+        });
+      });
+    }
+  };
+})(jQuery, Drupal);
