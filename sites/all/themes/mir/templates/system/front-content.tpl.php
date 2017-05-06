120e73be0068c3f6b6aebf83cc1a24fd825892df
Mohamed <mohamed.rez@gmail.com>
2017-05-06 14:50:05 +0100

    removed ask questions and relqted sites

--- sites/all/themes/mir/templates/system/front-content.tpl.php
+++ sites/all/themes/mir/templates/system/front-content.tpl.php
@@ -103,19 +103,6 @@
 
       <div class="block-wrapper">
         <div class="sub-title-wrapper">
-          <a href="/node/add/question" class="custom-button" style="background-color: #28919d;" title="أرسل سؤالك">
-            <i class="fa fa-question custom-button-icon" style="height:50px !important;"></i>
-            <span class="custom-button-wrap">
-            <span class="custom-button-title">أرسل سؤالك</span>
-            <span class="custom-button-tagline">نرحب باستقبال أسئلتكم عبر قسم الفتاوي</span>
-            </span>
-            <em></em>
-          </a>
-        </div>
-      </div>
-
-      <div class="block-wrapper">
-        <div class="sub-title-wrapper">
           <h2 class="sub-title">التغريدات</h2>
         </div>
         <div class="tweet-feed hidden-xs">
@@ -123,59 +110,6 @@
         </div>
       </div>
 
-      <div class="block-wrapper">
-        <div class="sub-title-wrapper">
-          <h2 class="sub-title">مواقع ذات صلة</h2>
-        </div>
-        <a href="http://www.miraathpubs.net" class="custom-button" style="background-color: #0E8C1E;" title="ميراث الأنبياء (English)">
-          <img src="<?php print $theme_image_path?>english-site-icon.png" alt="" class="custom-button-icon" style="height:50px !important;">
-          <span class="custom-button-wrap">
-          <span class="custom-button-title">ميراث الأنبياء (English)</span>
-          <span class="custom-button-tagline">نرحب بكم في النسخة الانجليزية</span>
-          </span>
-          <em></em>
-        </a>
-        <a href="http://www.miraath.fr/" class="custom-button" style="background-color: #28919d;" title="ميراث الأنبياء (Français)">
-          <img src="<?php print $theme_image_path?>french-site-icon.png" alt="" class="custom-button-icon" style="height:50px !important;">
-          <span class="custom-button-wrap">
-          <span class="custom-button-title">ميراث الأنبياء (Français)</span>
-          <span class="custom-button-tagline">نرحب بكم في النسخة الفرنسية</span>
-          </span>
-          <em></em>
-        </a>
-          <a href="http://aicha.miraath.net" class="custom-button" style="background-color: #82001A;" title="موقع أم المؤمنين">
-          <img src="<?php print $theme_image_path?>aicha-site-icon.png" alt="" class="custom-button-icon" style="height:50px !important;">
-          <span class="custom-button-wrap">
-          <span class="custom-button-title">موقع أم المؤمنين</span>
-          <span class="custom-button-tagline">موقع خاص بأم المؤمنين عائشة رضي الله عنها</span>
-          </span>
-          <em></em>
-        </a>
-        <a href="" class="custom-button" style="background-color: #32000A;" title="موقع المكتبة">
-          <img src="<?php print $theme_image_path?>maktaba-site-icon.png" alt="" class="custom-button-icon" style="height:50px !important;">
-          <span class="custom-button-wrap">
-          <span class="custom-button-title">موقع المكتبة</span>
-          <span class="custom-button-tagline">نرحب بكم في موقع المكتبة الالكتروني</span>
-          </span>
-          <em></em>
-        </a>
-        <a href="http://miraath.net/sounds.php?cat=650" class="custom-button" style="background-color: #c5cace;" title="مجالس ميراث الأنبياء التأصيلية">
-          <img src="<?php print $theme_image_path?>majaliss-site-icon.png" alt="" class="custom-button-icon" style="height:50px !important;">
-          <span class="custom-button-wrap">
-          <span class="custom-button-title">مجالس ميراث الأنبياء التأصيلية</span>
-          <span class="custom-button-tagline">نرحب بكم في مجالس ميراث الأنبياء التأصيلية</span>
-          </span>
-          <em></em>
-        </a>
-        <a href="http://www.miraath.de" class="custom-button" style="background-color: #B48001;" title="موقع ميراث الألماني">
-          <img src="<?php print $theme_image_path?>german-site-icon.png" alt="" class="custom-button-icon" style="height:50px !important;">
-          <span class="custom-button-wrap">
-          <span class="custom-button-title">موقع ميراث الألماني</span>
-          <span class="custom-button-tagline">نرحب بكم في موقع ميراث الألماني</span>
-          </span>
-          <em></em>
-        </a>
-      </div>
     </div>
   </div>
 </div>
